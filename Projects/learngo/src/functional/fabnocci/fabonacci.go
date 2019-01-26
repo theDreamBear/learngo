dd3c4f3b260bd2a4fb70af5b19ece7672558ec6a
theDreamBear <512068167@qq.com>
2019-01-26 13:32:10 +0800

    Fabnacci

--- Projects/learngo/src/functional/fabnocci/fabonacci.go
+++ Projects/learngo/src/functional/fabnocci/fabonacci.go
@@ -1,9 +1,28 @@
 package fabnocci
 
-func Fabnocci() func() int {
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+// 一般实现
+func Fabnocci() Fab {
 	a, b := 0, 1
 	return func() int {
 		a, b = b, a+b
 		return a
 	}
+}
+
+//
+type Fab func() int
+
+func (fa Fab) Read(p []byte) (n int, err error) {
+	next := fa()
+	if next > 1000 {
+		return 0, io.EOF
+	}
+	s := fmt.Sprintf("%d\n", next)
+	return strings.NewReader(s).Read(p)
 }
--- Projects/learngo/src/functional/main.go
+++ Projects/learngo/src/functional/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"functional/fabnocci"
+	"io"
 )
 
-func main() {
-	var fn = fabnocci.Fabnocci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(fn())
+func printFileContent(reader io.Reader) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
 	}
+}
+
+func main() {
+	//var fn = fabnocci.Fabnocci()
+	//for i := 0; i < 10; i++ {
+	//	fmt.Println(fn())
+	//}
+
+	var fn1 = fabnocci.Fabnocci()
+	printFileContent(fn1)
+
 }
